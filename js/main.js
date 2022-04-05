e24a261a7c067b815694278b5f3d04da891d1af7
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-05 00:35:46 +0000

    issues-1-create-an-entry WIPP

--- js/main.js
+++ js/main.js
@@ -6,9 +6,29 @@ var inputElement = document.querySelector('#url');
 
 var imgSource = document.querySelector('img');
 
-function input() {
+function input(event) {
   // console.log(inputElement.value);
   imgSource.setAttribute('src', inputElement.value);
 }
 
 inputElement.addEventListener('input', input);
+
+var titleEl = document.querySelector('#title');
+var urlEl = document.querySelector('#url');
+var notesEl = document.querySelector('#notes');
+
+var inputValue = {
+  title: '',
+  url: '',
+  notes: ''
+};
+
+function submit(event) {
+  inputValue.title = titleEl.value;
+  inputValue.url = urlEl.value;
+  inputValue.notes = notesEl.value;
+  // console.log(inputValue);
+  event.preventDefault();
+}
+
+document.addEventListener('submit', submit);
