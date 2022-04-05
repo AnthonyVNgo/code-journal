ce1ea0213417081d5fd53af2cde497109dba45ad
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-05 00:01:58 +0000

    issue-1-create-an-entry WIP

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 /* global data */
 /* exported data */
+
+var inputElement = document.querySelector('#url');
+// console.log(inputElement);
+
+var imgSource = document.querySelector('img');
+
+function input() {
+  // console.log(inputElement.value);
+  imgSource.setAttribute('src', inputElement.value);
+}
+
+inputElement.addEventListener('input', input);
