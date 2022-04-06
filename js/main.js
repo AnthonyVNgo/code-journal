95d2940a665e023cade89e77fcffb1a26100ec07
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-06 00:23:47 +0000

    issue-2-view-entries WIP

--- js/main.js
+++ js/main.js
@@ -1,2 +1,48 @@
 /* global data */
 /* exported data */
+
+// function createEntry(entry) {
+//   var entriesListEl = document.querySelector('#entries-list');
+
+//   var liEl = document.createElement('li');
+//   var imgEl = document.createElement('img');
+//   var divEl = document.createElement('div');
+//   var ptitleEl = document.createElement('p');
+//   var ptextEl = document.createElement('p');
+
+//   liEl.setAttribute('class', 'row');
+//   imgEl.setAttribute('class', 'column-half');
+//   // imgEl.setAttribute('src', test.url);
+//   divEl.setAttribute('class', 'column-half');
+//   ptitleEl.setAttribute('class', 'title');
+//   ptitleEl.textContent = test.title;
+//   ptextEl.setAttribute('class', 'text');
+//   ptextEl.textContent = test.notes;
+
+//   liEl.appendChild(imgEl);
+//   liEl.appendChild(divEl);
+//   divEl.appendChild(ptitleEl);
+//   divEl.appendChild(ptextEl);
+
+//   entriesListEl.appendChild(liEl);
+// }
+
+// <li class="row">
+//   <img class="column-half" src="images/placeholder-image-square.jpg" alt="">
+//     <div class="column-half">
+//       <p class="title">Ada Lovelace</p>
+//       <p class="text">
+
+// var inputValue = {
+//   title: titleEl.value,
+//   url: urlEl.value,
+//   notes: notesEl.value,
+//   ID: data.nextEntryId
+// };
+
+// var test = {
+//   title: '1',
+//   url: '1',
+//   notes: '1',
+//   ID: data.nextEntryId
+// };
