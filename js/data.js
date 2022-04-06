43e356f726ec9e031f16cb46c03eac1addc0a52d
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-06 00:48:28 +0000

    issue-2-view-entries WIPP

--- js/data.js
+++ js/data.js
@@ -6,3 +6,26 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+data.entries = [
+  {
+    title: '1',
+    url: 'images/placeholder-image-square.jpg',
+    notes: '1',
+    ID: 1
+  },
+
+  {
+    title: '2',
+    url: 'images/placeholder-image-square.jpg',
+    notes: '2',
+    ID: 2
+  },
+
+  {
+    title: '2',
+    url: 'images/placeholder-image-square.jpg',
+    notes: '2',
+    ID: 2
+  }
+];
--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
 /* global data */
 /* exported data */
 
-// function createEntry(entry) {
-//   var entriesListEl = document.querySelector('#entries-list');
+function createEntry(entry) {
+  var entriesListEl = document.querySelector('#entries-list');
 
-//   var liEl = document.createElement('li');
-//   var imgEl = document.createElement('img');
-//   var divEl = document.createElement('div');
-//   var ptitleEl = document.createElement('p');
-//   var ptextEl = document.createElement('p');
+  var liEl = document.createElement('li');
+  var imgEl = document.createElement('img');
+  var divEl = document.createElement('div');
+  var ptitleEl = document.createElement('p');
+  var ptextEl = document.createElement('p');
 
-//   liEl.setAttribute('class', 'row');
-//   imgEl.setAttribute('class', 'column-half');
-//   // imgEl.setAttribute('src', test.url);
-//   divEl.setAttribute('class', 'column-half');
-//   ptitleEl.setAttribute('class', 'title');
-//   ptitleEl.textContent = test.title;
-//   ptextEl.setAttribute('class', 'text');
-//   ptextEl.textContent = test.notes;
+  liEl.setAttribute('class', 'row');
+  imgEl.setAttribute('class', 'column-half');
+  imgEl.setAttribute('src', test.url);
+  divEl.setAttribute('class', 'column-half');
+  ptitleEl.setAttribute('class', 'title');
+  ptitleEl.textContent = test.title;
+  ptextEl.setAttribute('class', 'text');
+  ptextEl.textContent = test.notes;
 
-//   liEl.appendChild(imgEl);
-//   liEl.appendChild(divEl);
-//   divEl.appendChild(ptitleEl);
-//   divEl.appendChild(ptextEl);
+  liEl.appendChild(imgEl);
+  liEl.appendChild(divEl);
+  divEl.appendChild(ptitleEl);
+  divEl.appendChild(ptextEl);
 
-//   entriesListEl.appendChild(liEl);
-// }
+  entriesListEl.appendChild(liEl);
+}
+
+function contentLoaded(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    createEntry(data.entries[i]);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', contentLoaded);
 
 // <li class="row">
 //   <img class="column-half" src="images/placeholder-image-square.jpg" alt="">
@@ -40,9 +48,9 @@
 //   ID: data.nextEntryId
 // };
 
-// var test = {
-//   title: '1',
-//   url: '1',
-//   notes: '1',
-//   ID: data.nextEntryId
-// };
+var test = {
+  title: 'adngfsjodfosinsddso',
+  url: 'images/placeholder-image-square.jpg',
+  notes: 'spdofjaoighojfaojsfigosjgifo',
+  ID: data.nextEntryId
+};
