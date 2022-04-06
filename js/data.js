9a38c0be5f1f66ff74c2d34de9eb8959170a7d59
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-06 22:46:09 +0000

    completed issue-2-view-entries

--- js/data.js
+++ js/data.js
@@ -7,25 +7,15 @@ var data = {
   nextEntryId: 1
 };
 
-data.entries = [
-  {
-    title: 'Vanilla Patrick',
-    url: 'https://i.kym-cdn.com/entries/icons/original/000/009/803/spongebob-squarepants-patrick-spongebob-patrick-star-background-225039.jpg',
-    notes: 'Yee',
-    ID: 1
-  },
+function saveToLocalStorage(event) {
+  var dataModelJSON = JSON.stringify(data);
+  localStorage.setItem('data model', dataModelJSON);
+}
 
-  {
-    title: 'SpongeGar',
-    url: 'https://i.kym-cdn.com/entries/icons/mobile/000/018/259/55181940.jpg',
-    notes: 'hoohoeohehoehoe',
-    ID: 2
-  },
+window.addEventListener('submit', saveToLocalStorage);
 
-  {
-    title: 'EvilPatrick',
-    url: 'https://i.kym-cdn.com/entries/icons/facebook/000/025/580/Screen_Shot_2018-03-01_at_12.59.37_PM.jpg',
-    notes: 'hehe',
-    ID: 3
-  }
-];
+var savedDataModelJSON = localStorage.getItem('data model');
+
+if (savedDataModelJSON !== null) {
+  data = JSON.parse(savedDataModelJSON);
+}
--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@
 
 function createEntry(entry) {
   var entriesListEl = document.querySelector('#entries-list');
-
   var liEl = document.createElement('li');
+  var divImgEl = document.createElement('div');
   var imgEl = document.createElement('img');
   var divEl = document.createElement('div');
   var ptitleEl = document.createElement('p');
   var ptextEl = document.createElement('p');
 
   liEl.setAttribute('class', 'row');
-  imgEl.setAttribute('class', 'column-half');
+  divImgEl.setAttribute('class', 'column-half');
   imgEl.setAttribute('src', entry.url);
   divEl.setAttribute('class', 'column-half');
   ptitleEl.setAttribute('class', 'title');
@@ -19,7 +19,35 @@ function createEntry(entry) {
   ptextEl.setAttribute('class', 'text');
   ptextEl.textContent = entry.notes;
 
-  liEl.appendChild(imgEl);
+  liEl.appendChild(divImgEl);
+  divImgEl.appendChild(imgEl);
+  liEl.appendChild(divEl);
+  divEl.appendChild(ptitleEl);
+  divEl.appendChild(ptextEl);
+  entriesListEl.prepend(liEl);
+  // entriesListEl.appendChild(liEl);
+}
+
+function createEntry2(entry) {
+  var entriesListEl = document.querySelector('#entries-list');
+  var liEl = document.createElement('li');
+  var divImgEl = document.createElement('div');
+  var imgEl = document.createElement('img');
+  var divEl = document.createElement('div');
+  var ptitleEl = document.createElement('p');
+  var ptextEl = document.createElement('p');
+
+  liEl.setAttribute('class', 'row');
+  divImgEl.setAttribute('class', 'column-half');
+  imgEl.setAttribute('src', entry.url);
+  divEl.setAttribute('class', 'column-half');
+  ptitleEl.setAttribute('class', 'title');
+  ptitleEl.textContent = entry.title;
+  ptextEl.setAttribute('class', 'text');
+  ptextEl.textContent = entry.notes;
+
+  liEl.appendChild(divImgEl);
+  divImgEl.appendChild(imgEl);
   liEl.appendChild(divEl);
   divEl.appendChild(ptitleEl);
   divEl.appendChild(ptextEl);
@@ -28,28 +56,64 @@ function createEntry(entry) {
 
 function contentLoaded(event) {
   for (var i = 0; i < data.entries.length; i++) {
-    createEntry(data.entries[i]);
+    createEntry2(data.entries[i]);
   }
 }
 
 document.addEventListener('DOMContentLoaded', contentLoaded);
 
-// <li class="row">
-//   <img class="column-half" src="images/placeholder-image-square.jpg" alt="">
-//     <div class="column-half">
-//       <p class="title">Ada Lovelace</p>
-//       <p class="text">
+var newEntryButton = document.querySelector('.new-button');
 
-// var inputValue = {
-//   title: titleEl.value,
-//   url: urlEl.value,
-//   notes: notesEl.value,
-//   ID: data.nextEntryId
-// };
+function viewSwap(event) {
+  event.preventDefault();
 
-// var test = {
-//   title: 'adngfsjodfosinsddso',
-//   url: 'images/placeholder-image-square.jpg',
-//   notes: 'spdofjaoighojfaojsfigosjgifo',
-//   ID: data.nextEntryId
-// };
+  var entriesDivEl = document.querySelector('#entries-container');
+  entriesDivEl.setAttribute('class', 'hidden');
+
+  var entriesFormEl = document.querySelector('#entries-form-container');
+  entriesFormEl.setAttribute('class', 'container');
+}
+
+newEntryButton.addEventListener('click', viewSwap);
+
+var inputElement = document.querySelector('#url');
+var imgSource = document.querySelector('img');
+
+function input(event) {
+  imgSource.setAttribute('src', inputElement.value);
+}
+
+inputElement.addEventListener('input', input);
+
+var formElement = document.querySelector('form');
+var titleEl = document.querySelector('#title');
+var urlEl = document.querySelector('#url');
+var notesEl = document.querySelector('#notes');
+var newEntryImgEl = document.querySelector('#new-entry-image');
+
+function submit(event) {
+  event.preventDefault();
+
+  var inputValue = {
+    title: titleEl.value,
+    url: urlEl.value,
+    notes: notesEl.value,
+    ID: data.nextEntryId
+  };
+
+  // data.entries.push(inputValue);
+  data.entries.unshift(inputValue);
+  data.nextEntryId++;
+  newEntryImgEl.setAttribute('src', 'images/placeholder-image-square.jpg');
+  formElement.reset();
+
+  var entriesDivEl = document.querySelector('#entries-container');
+  entriesDivEl.setAttribute('class', 'container');
+
+  var entriesFormEl = document.querySelector('#entries-form-container');
+  entriesFormEl.setAttribute('class', 'hidden');
+
+  createEntry(data.entries[0]);
+}
+
+document.addEventListener('submit', submit);
